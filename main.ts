f37aa328cdd46184ffe36af08f742299a8659461
Nouman Nadeem <160357491+NoumanNadeem123@users.noreply.github.com>
2024-03-12 02:54:16 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+//*Favorite Number: Store your favorite number in a variable. Then, using that variable, create a
+//message that reveals your favorite number. Print that message.*//
+
+
+//let favoriteNumber: number = 7
+//console.log(`My favorite Number is ${favoriteNumber}`);
+
+let favoriteNumber: number = 1
+console.log(`1st Ramadan Mubarak Tommorrow is ${favoriteNumber}st fasting day`);
+
